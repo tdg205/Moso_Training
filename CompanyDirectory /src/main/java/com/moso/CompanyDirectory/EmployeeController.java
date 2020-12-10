1d2045f7e1f85fb8e610d2824101b69fd98cdda2
Truong Dinh Giang <giang.itk10@gmail.com0>
2020-12-10 12:42:33 +0700

    5. Fix lỗi show data UI ListEmployeeComponent:        @CrossOrigin(origins = "http://localhost:3000")

--- CompanyDirectory /src/main/java/com/moso/CompanyDirectory/EmployeeController.java
+++ CompanyDirectory /src/main/java/com/moso/CompanyDirectory/EmployeeController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
  * 
  * @author GiangTD
  */
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1")
 public class EmployeeController {
